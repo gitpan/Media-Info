07225951a3eac38d7355187591e1ee9349d6f8b2
Steven Haryanto <stevenharyanto@gmail.com>
2014-07-22 15:35:53 +0000

    Import of SHARYANTO/Media-Info-0.06 from CPAN.
    gitpan-cpan-distribution: Media-Info
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHARYANTO/Media-Info-0.06.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Media/Info.pm
+++ lib/Media/Info.pm
@@ -10,7 +10,7 @@ our @EXPORT_OK = qw(
                        get_media_info
                );
 
-our $VERSION = '0.05'; # VERSION
+our $VERSION = '0.06'; # VERSION
 
 our %SPEC;
 
@@ -47,7 +47,7 @@ Media::Info - Return information on media (music, video, etc) file/URL
 
 =head1 VERSION
 
-version 0.05
+This document describes version 0.06 of Media::Info (from Perl distribution Media-Info), released on 2014-07-22.
 
 =head1 SYNOPSIS
 
@@ -98,7 +98,16 @@ Media file/URL.
 
 Return value:
 
-Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+ (any)
 
 =head1 SEE ALSO
 
